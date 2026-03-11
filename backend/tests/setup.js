edd510e762354e7319eb695c7633493dd5f4f075
Your Name <you@example.com>
2026-03-11 16:57:42 +0530

    FINAL

--- backend/tests/setup.js
+++
@@ -1,8 +0,0 @@
-// tests/setup.js - Test environment setup
-const mongoose = require("mongoose");
-
-// Clean up after all tests
-afterAll(async () => {
-    // Close the MongoDB connection
-    await mongoose.connection.close();
-});
